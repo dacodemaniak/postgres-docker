0a0ae632397f738ef404d99888278688f5deede7
matthcol <matthias.colin@gmail.com>
2022-12-12 16:20:53 +0100

    add FK in table trainees

--- sql/001-dbpoe-ddl.sql
+++ sql/001-dbpoe-ddl.sql
@@ -1,18 +1,3 @@
-CREATE TABLE trainees (
-    id serial,
-    lastname varchar(50) NOT NULL,
-    firstname varchar(50) NOT NULL,
-    gender char(1),
-    birthdate date NOT NULL,
-    email varchar(100) NOT NULL,
-    phone_number varchar(15),
-	CONSTRAINT pk_trainees PRIMARY KEY(id)
-);
-
-ALTER TABLE trainees
-    ADD CONSTRAINT uniq_trainees_email 
-	UNIQUE (email);
-
 CREATE TABLE poes(
 	id serial,
 	title varchar(150) NOT NULL,
@@ -22,6 +7,27 @@ CREATE TABLE poes(
 	CONSTRAINT pk_poes PRIMARY KEY(id)
 );
 
+CREATE TABLE trainees (
+    id serial,
+    lastname varchar(50) NOT NULL,
+    firstname varchar(50) NOT NULL,
+    gender char(1),
+    birthdate date NOT NULL,
+    email varchar(100) NOT NULL,
+    phone_number varchar(15),
+    poe_id integer,
+	CONSTRAINT pk_trainees PRIMARY KEY(id)
+);
+
 ALTER TABLE poes
     ADD CONSTRAINT chk_poes_dates 
 	CHECK (begin_date < end_date);
+
+ALTER TABLE trainees
+    ADD CONSTRAINT uniq_trainees_email 
+	UNIQUE (email);
+
+ALTER TABLE trainees 
+    ADD CONSTRAINT fk_trainees_poe
+    FOREIGN KEY (poe_id)
+    REFERENCES poes(id);
